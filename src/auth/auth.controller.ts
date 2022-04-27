3205a9dfddff70e8267398f464971dda56a11d0a
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-04-27 23:14:06 +0300

    describe auth controller methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @HttpCode(200)
+  @Post('register')
+  async register(@Body() dto: AuthDto) {}
+
+  @HttpCode(200)
+  @Post('login')
+  async post(@Body() dto: AuthDto) {}
+}
