85cb1386347ee3760a9be2dc0373cdbc5b29b194
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-04-29 20:35:06 +0300

    finish review model

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,20 @@
-export class ReviewModel {
-  _id: string;
+import { Prop, Schema } from '@nestjs/mongoose';
+import { BasicModel } from 'src/common/basic.model';
+
+@Schema()
+export class ReviewModel extends BasicModel {
+  @Prop()
   name: string;
+
+  @Prop()
   description: string;
+
+  @Prop()
   title: string;
+
+  @Prop()
   rating: number;
+
+  @Prop()
   createdAt: Date;
 }
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewController } from './review.controller';
+import { ReviewModel } from './review.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Review',
+        schema: ReviewModel,
+      },
+    ]),
+  ],
   controllers: [ReviewController],
 })
 export class ReviewModule {}
