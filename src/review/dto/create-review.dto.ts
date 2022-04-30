15fbeb07390f708e8cf67cbf7104b1ba2aaff41f
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-04-30 12:59:13 +0300

    add validate decorators for create-review.dto

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+  @IsString({ message: 'Не передано name' })
   name: string;
+
+  @IsString({ message: 'Не передано description' })
   description: string;
+
+  @IsString({ message: 'Не переданый title' })
   title: string;
+
+  @IsNumber(undefined, { message: 'Не переданный rating' })
+  @Min(1, { message: 'Минимальное значение 1' })
+  @Max(5, { message: 'Максимальное значение 5' })
   rating: number;
+
+  @IsNumber(undefined, { message: 'Не переданный rating' })
   productId: string;
 }
