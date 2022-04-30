9cfc0d861edc8e8ef08cf08a037da1699915c2d1
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-04-30 22:38:43 +0300

    finish login and coverage by test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { UserSchema } from './user.model';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getJWTConfig } from 'src/config/jwt.config';
+import { getJWTConfig } from '../config/jwt.config';
 @Module({
   controllers: [AuthController],
   imports: [
--- test/auth.controller.e2e-spec.ts
+++ test/auth.controller.e2e-spec.ts
@@ -1,11 +1,15 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { AuthService } from './../src/auth/auth.service';
 import * as request from 'supertest';
 import { AuthDto } from './../src/auth/dto/auth.dto';
 import { disconnect } from 'mongoose';
-import { USER_EXISTS } from './../src/auth/auth.constants';
+import {
+  USER_EXISTS,
+  USER_NOT_FOUND,
+  USER_WRONG_PASSWORD,
+} from './../src/auth/auth.constants';
 
 const authDto: AuthDto = {
   login: 'test1@gmail.com',
@@ -54,6 +58,44 @@ describe('AuthController (e2e)', () => {
     expect(statusCode).toBe(400);
     expect(body.error).toBeTruthy();
     expect(body.message.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('/auth/login/ (POST) - success', async () => {
+    await request(app.getHttpServer()).post('/auth/register/').send(authDto);
+    const { body, statusCode } = await request(app.getHttpServer())
+      .post('/auth/login/')
+      .send(authDto);
+
+    expect(statusCode).toBe(200);
+    expect(body.token).toBeTruthy();
+  });
+
+  it('/auth/login/ (POST) - failed, invalid data', async () => {
+    const { body, statusCode } = await request(app.getHttpServer())
+      .post('/auth/login/')
+      .send({ ...authDto, login: 123123 });
+
+    expect(statusCode).toBe(400);
+    expect(body.error).toBeTruthy();
+    expect(body.message.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('/auth/login/ (POST) - failed, user not found', async () => {
+    const { body, statusCode } = await request(app.getHttpServer())
+      .post('/auth/login/')
+      .send({ login: 'test25@gmail.com', password: 'asdas' });
+
+    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
+    expect(body.message).toBe(USER_NOT_FOUND);
+  });
+
+  it('/auth/login/ (POST) - failed, incorrect password', async () => {
+    const { body, statusCode } = await request(app.getHttpServer())
+      .post('/auth/login/')
+      .send({ ...authDto, password: 'test12312' });
+
+    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
+    expect(body.message).toBe(USER_WRONG_PASSWORD);
   });
 
   afterAll(async () => {
