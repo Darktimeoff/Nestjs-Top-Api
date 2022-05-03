e4e125ee58055eeda8f547b11cfd6713bf277989
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-05-03 17:51:19 +0300

    add base deps for sitemap controller

--- src/sitemap/sitemap.controller.ts
+++ src/sitemap/sitemap.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TopPageService } from 'src/top-page/top-page.service';
 
 @Controller('sitemap')
-export class SitemapController {}
+export class SitemapController {
+  domain: string;
+
+  constructor(
+    private readonly topPageService: TopPageService,
+    private readonly configService: ConfigService,
+  ) {
+    this.domain = this.configService.get('DOMAIN') ?? '';
+  }
+}
